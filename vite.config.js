7d5cea5bd698b3e9f55a4365e4540476bd9d7e7f
Eduardo Szchaida <90700227+duduszchaida@users.noreply.github.com>
2024-07-25 00:06:33 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: 
-      '/api': {
-        target: 'https://api-liturgia-diaria.vercel.app',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
-      }
-    }
-  }
-});
+})

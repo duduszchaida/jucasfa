7a0ce38fbb5ca235eeb317e4307a71c75ec0dd44
duduszchaida <duduszchaida12@gmail.com>
2025-01-28 11:23:36 -0300

    atualização cooredenação

--- src/components/principal.jsx
+++ src/components/principal.jsx
@@ -7,16 +7,19 @@ import 'primereact/resources/themes/saga-blue/theme.css'; // tema primereact
 import 'primereact/resources/primereact.min.css'; // componentes primereact
 import 'primeicons/primeicons.css'; // ícones primereact
 import { Button } from 'primereact/button';
+import CarnavalContador from './CarnavalContador';
 
 function Principal() {
   
   
   const eventos = [
-    { id: 1, img: "rocha.jpg", title: "3º Rocha", link: "https://www.facebook.com/media/set/?set=a.472926738206430&type=3" },
+    { id: 1, img: "quarto-rocha.jpg", title: "4º Rocha", link: "https://drive.google.com/drive/folders/1KZmtEvXLH9WEvxtlLR95_95cCcimamSP" },
     { id: 2, img: "acampacasfa.jpg", title: "1º AcampaCasfa", link: "https://drive.google.com/drive/folders/1Dnw9NdgL--ApPUMGc1e6hBBUAwEyHiGp" },
     { id: 3, img: "gincana-interna.jpeg", title: "7ª Gincana Interna", link: "https://drive.google.com/drive/folders/17HuN8inflvpj2zU5hvCbwiwgIVM3tVVT" },
     { id: 4, img: "niver-jucao.jpg", title: "27º Aniversário do Jucasfa", link: "https://drive.google.com/drive/folders/1HWMoChvujs7Um97_D2eu-4w0rYeWNrcG" },
-    { id: 5, img: "gincana-externa.jpg", title: "14ª Gincana Externa", link: "https://www.facebook.com/media/set/?set=a.618685883630514&type=3" }
+    { id: 5, img: "gincana-externa.jpg", title: "14ª Gincana Externa", link: "https://www.facebook.com/media/set/?set=a.618685883630514&type=3" },
+    { id: 6, img: "rocha.jpg", title: "3º Rocha", link: "https://www.facebook.com/media/set/?set=a.472926738206430&type=3" },
+    { id: 7, img: "jovem-noel.jpg", title: "Jovem Noel 2024", link: "https://drive.google.com/drive/folders/1KZmtEvXLH9WEvxtlLR95_95cCcimamSP" }
   ];
 
   const itemTemplate = (item) => {
@@ -82,7 +85,9 @@ useEffect(() => {
 
   return (
     <>
+   
       <section id="inicio" className="banner-area">
+      
         <div className="banner-img"></div>
         <h3>Juventude Católica São Francisco de Assis</h3>
         <h1><span>Jucasfa</span></h1>
@@ -135,12 +140,12 @@ useEffect(() => {
           <li className='col-4 xl:col-2 p-0 xl:p-3'>
             <i className="pi pi-mobile text-4xl xl:text-6xl mb-2 xl:mb-4"></i>
             <p className='line-height-2 text-mob xl:text-base p-1 xl:p-3'>
-            Paty (45)99155-0933        <h4>Coordenadora</h4></p>
+            Ana Dametto (45)98805-1545        <h4>Coordenadora</h4></p>
               
           </li>
           <li className='col-4 xl:col-2 p-0 xl:p-3'>
             <i className="pi pi-dollar text-4xl xl:text-6xl mb-2 xl:mb-4"></i>
-            <p className='line-height-2 text-mob xl:text-base p-1 xl:p-3'>Pix:(45)99902-3640      <h4>Eduardo H.</h4></p>
+            <p className='line-height-2 text-mob xl:text-base p-1 xl:p-3'>Pix: 07970507956      <h4>Eduarda Dallacort</h4></p>
        
           </li>
         </ul>
